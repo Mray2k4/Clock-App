79a23c2b971c343b990a2ea3422a4d89b0e32433
Marque Ray <mray2k4@yahoo.com>
2023-04-20 19:19:09 -0700

    added day and month words

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 let newDate = new Date();
 
-let date = newDate.getMonth() +1  + ' - ' + newDate.getDate() + ' - ' + newDate.getFullYear();
+let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+let months = ['January', 'Feburay', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+let date = days[newDate.getDay()] + ',' + ' '  + months[newDate.getMonth()]  + ' ' + newDate.getDate() + ' ' + newDate.getFullYear();
 document.getElementById('todaydate').innerHTML = date;
 
+setInterval(function() {
 let time = newDate.getHours() + ':' + newDate.getMinutes() + ':' + newDate.getSeconds();
 document.getElementById('todaytime').innerHTML = time;
+}, 1000)
+
+//Date and Time Display
+let d = new Date("October 13, 2014");
+document.getElementById('clock').innerHTML = d;
+
+
 /*
 let count = 0;
 let clock = document.getElementById('todaytime');
