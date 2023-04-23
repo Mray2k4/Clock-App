60d88aee433dd5a80ad2bf72c1410c6ada78caaf
Marque Ray <mray2k4@yahoo.com>
2023-04-23 09:50:26 -0700

    finishing touches

--- script.js
+++ script.js
@@ -4,19 +4,45 @@ let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'S
 
 let months = ['January', 'Feburay', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
+// Month, Date, Year
 let date = days[newDate.getDay()] + ',' + ' '  + months[newDate.getMonth()]  + ' ' + newDate.getDate() + ' ' + newDate.getFullYear();
 document.getElementById('todaydate').innerHTML = date;
 
-
+// Clock 
 function clock() {
   let newDate = new Date();
   let hours = newDate.getHours();
   let min = newDate.getMinutes();
   let sec = newDate.getSeconds();
-  let ampm = hours >= 12 ? "PM" : "AM";
-  min = min < 10 ? '0' + min : min;
-  sec = sec < 10 ? '0' + sec : sec;
-  hours = ((hours % 12 || 12) < 10 ? '0' : '') + (hours % 12 || 12);
+
+// Hours with leading zero
+  if (hours < 12) {
+    hours = '0' + hours;
+  } else {
+    hours;
+  }
+
+  // Minutes with leading zero
+  if (min < 10) {
+    min = '0' + min;
+  } else {
+    min;
+  };
+
+  // Seconds with leading zero
+  if (sec < 10) {
+    sec = '0' + sec;
+  } else {
+    sec;
+  }
+
+  // AM/PM
+  if (hours >= 12) {
+    ampm = 'PM';
+  } else {
+    ampm = 'AM';
+  };
+
   let time = hours + ':' + min + ':' + sec + ' ' + ampm;
 document.getElementById('todaytime').innerHTML = time;
 }
