4c0dd3b78ae6ad8e6aabafdf801cd2c3a2896877
Marque Ray <mray2k4@yahoo.com>
2023-04-22 23:44:25 -0700

    added clock format

--- script.js
+++ script.js
@@ -10,7 +10,14 @@ document.getElementById('todaydate').innerHTML = date;
 
 function clock() {
   let newDate = new Date();
-  let time = newDate.getHours() + ':' + newDate.getMinutes() + ':' + newDate.getSeconds();
+  let hours = newDate.getHours();
+  let min = newDate.getMinutes();
+  let sec = newDate.getSeconds();
+  let ampm = hours >= 12 ? "PM" : "AM";
+  min = min < 10 ? '0' + min : min;
+  sec = sec < 10 ? '0' + sec : sec;
+  hours = ((hours % 12 || 12) < 10 ? '0' : '') + (hours % 12 || 12);
+  let time = hours + ':' + min + ':' + sec + ' ' + ampm;
 document.getElementById('todaytime').innerHTML = time;
 }
 setInterval(clock, 1000);
